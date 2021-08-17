d5eed2ea533dd80eb5ebbbc07260762e79f9b13d
saiful-dev <saiful.hstu.sii@gmail.com>
2021-08-17 22:26:27 +0600

    ShouldComponenetUpdate Added

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,42 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary =(props)=>{
+class OrderSummary extends Component{
+    // this could be functional component, doesn't add class
+    componentDidUpdate(){
+        console.log('[OrderSummary.js] Didupdate')
+    }
 
-    const ingredientSummary =Object.keys(props.ingredients)
+    render(){
+
+        const ingredientSummary =Object.keys(this.props.ingredients)
                     .map(igkey =>{
                         return (
                             <li> 
-                                <span style={{textTransform: 'capitalize',textAlign:'left'}}> {igkey}</span>: {props.ingredients[igkey]}
+                                <span style={{textTransform: 'capitalize',textAlign:'left'}}> 
+                                {igkey}</span>: {this.props.ingredients[igkey]}
                             </li>);
                     });
+        return(
+            <Aux>
+                <h3>Your Order</h3>
+                <p> A delicious burger with following ingredients</p>
+                <ul>
+                    {
+                        ingredientSummary
+                    }
+                </ul>
+                <p style={{'color':'black'}}><strong>Total Price: {this.props.price}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType={'Danger'} clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType={'Success'} clicked={this.props.purchaseContinued}>Continue</Button>
+            </Aux>
 
-    return( 
-        <Aux>
-            <h3>Your Order</h3>
-            <p> A delicious burger with following ingredients</p>
-            <ul>
-                {
-                    ingredientSummary
-                }
-            </ul>
-            <p style={{'color':'black'}}><strong>Total Price: {props.price}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType={'Danger'} clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType={'Success'} clicked={props.purchaseContinued}>Continue</Button>
-        </Aux>
-    );
-};
+        );
+    }
+}
 
-export default orderSummary;
+
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,24 +1,38 @@
 
-import React from 'react';
+import React,{Component} from 'react';
 import './Modal.css'
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal =(props) =>(
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        
-        <div 
-            className='Modal'
-            style={{
-                transform: props.show? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show? '1':'0',
-            }}>
+class Modal extends Component{
+    shouldComponentUpdate(nextProps,nextState){ //return false to tell React the update can be skipped
+        return nextProps.show !== this.props.show;
+    }
+    componentDidUpdate(){
+        console.log('[Modal] Will update')
+    }
+    render(){
+        return(
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                
+                <div 
+                    className='Modal'
+                    style={{
+                        transform: this.props.show? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show? '1':'0',
+                    }}>
 
-            {props.children}
-        </div> 
-    </Aux>
+                    {this.props.children}
+                </div> 
+            </Aux>
+
+        );
+    }
+}
+
+    
         // props.children=>it is used to display whatever you include between the opening and closing tags when invoking a component.
-);
-export default modal;
+
+export default Modal;
 
